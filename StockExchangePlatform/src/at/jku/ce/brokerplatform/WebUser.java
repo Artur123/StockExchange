3e61b56a801a2c3db5fc8ab411fdf329e845a4a5
Nicole <Nicole@10.0.0.4>
2014-01-26 15:14:39 +0100

    changed user

--- StockExchangePlatform/src/at/jku/ce/brokerplatform/WebUser.java
+++ StockExchangePlatform/src/at/jku/ce/brokerplatform/WebUser.java
@@ -3,10 +3,20 @@ package at.jku.ce.brokerplatform;
 public class WebUser {
 
 	private String name;
+	private String password;
 
-
-	public WebUser(String name) {
+	public WebUser(String name,String password) {
 		this.name = name;
+		this.password = password;
+		Depot depot = new Depot();
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	public String getName() {
