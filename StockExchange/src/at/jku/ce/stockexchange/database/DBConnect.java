aa6a3de31480b1948d62ce3e856010af909a2e70
Artur123 <20062080@atn.nu>
2014-01-29 17:58:41 +0100

    added jdbc driver

--- StockExchange/src/at/jku/ce/stockexchange/database/DBConnect.java
+++ StockExchange/src/at/jku/ce/stockexchange/database/DBConnect.java
@@ -1,22 +1,28 @@
-package at.jku.ce.brokerplatform;
+package at.jku.ce.stockexchange.database;
 import java.sql.*;
-import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
 
 import at.jku.ce.stockexchange.service.Exchange;
 import at.jku.ce.stockexchange.service.Stock;
 import at.jku.ce.stockexchange.service.StockExchange;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.GregorianCalendar;
-
-import javax.xml.datatype.DatatypeConfigurationException;
-
 public class DBConnect {
 	
+	
 	private static Connection getConnection() throws SQLException, ClassNotFoundException{
-		Class.forName("com.mysql.jdbc.Driver");
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return DriverManager.getConnection("jdbc:mysql://140.78.73.67:3306/stockexchangeDB","uece","cestock2013");
 	}
 	
@@ -55,13 +61,13 @@ public class DBConnect {
 		}
 	}
 	
-	public static List<Exchange> getExchanges(String mic) {
+	public static List<Exchange> getExchanges(StockExchange stockExchange) {
 		List<Exchange> exchanges = new ArrayList<Exchange>();
 		Connection cn = null;
 		try {
 			cn = getConnection();
 			PreparedStatement pstmt = cn.prepareStatement("SELECT transaction_id,mic,stockexchange_name,isin,stock_name,stock_currency,stock_price,order,execution,exchange_date FROM exchange WHERE mic=?");
-			pstmt.setString(1,mic);
+			pstmt.setString(1,stockExchange.getMic());
 			
 			ResultSet rs=pstmt.executeQuery();
 			while(rs.next()){		 	
@@ -70,14 +76,13 @@ public class DBConnect {
 			 	newStock.setName(rs.getString("stock_name"));
 			 	newStock.setPrice(rs.getDouble("stock_price"));
 			 	newStock.setCurrency(rs.getString("stock_currency"));
-			 	//Annahme: Publication-Date und availability sind bei der Auflistung nicht gefragt.
+			 	//no availability or publication
 			 	
 			 	Exchange newExchange = new Exchange();
 			 	newExchange.setStock(newStock);
 			 	newExchange.setOrder(rs.getInt("order"));
 			 	newExchange.setExecution(rs.getInt("execution"));
-			 	StockExchange newse = new StockExchange(mic, mic);
-			 	newExchange.setStockExchange(newse);
+			 	newExchange.setStockExchange(stockExchange);
 				GregorianCalendar cal = new GregorianCalendar();
 				cal.setTime(rs.getDate("exchange_date"));
 				newExchange.setExchangeDate(javax.xml.datatype.DatatypeFactory.newInstance().newXMLGregorianCalendar(cal));
